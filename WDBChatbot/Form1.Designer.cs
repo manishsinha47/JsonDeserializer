bea84b8e01aadeb0eeb81d1f41fb61dc5eea137c
Manish Sinha <https://github.com/manishsinha47>
2018-03-15 20:40:07 +0530

    Only Extraction happening right now

---
+++ WDBChatbot/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WDBChatbot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.jsonInput = new System.Windows.Forms.TextBox();
+            this.jsonDeserialize = new System.Windows.Forms.Button();
+            this.jsonOutput = new System.Windows.Forms.TextBox();
+            this.jsonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // jsonInput
+            // 
+            this.jsonInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.jsonInput.Location = new System.Drawing.Point(12, 12);
+            this.jsonInput.Multiline = true;
+            this.jsonInput.Name = "jsonInput";
+            this.jsonInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.jsonInput.Size = new System.Drawing.Size(776, 159);
+            this.jsonInput.TabIndex = 0;
+            // 
+            // jsonDeserialize
+            // 
+            this.jsonDeserialize.Location = new System.Drawing.Point(12, 193);
+            this.jsonDeserialize.Name = "jsonDeserialize";
+            this.jsonDeserialize.Size = new System.Drawing.Size(124, 57);
+            this.jsonDeserialize.TabIndex = 1;
+            this.jsonDeserialize.Text = "Deserialize";
+            this.jsonDeserialize.UseVisualStyleBackColor = true;
+            this.jsonDeserialize.Click += new System.EventHandler(this.jsonDeserialize_Click);
+            // 
+            // jsonOutput
+            // 
+            this.jsonOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.jsonOutput.Location = new System.Drawing.Point(12, 275);
+            this.jsonOutput.Multiline = true;
+            this.jsonOutput.Name = "jsonOutput";
+            this.jsonOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.jsonOutput.Size = new System.Drawing.Size(776, 159);
+            this.jsonOutput.TabIndex = 2;
+            // 
+            // jsonClear
+            // 
+            this.jsonClear.Location = new System.Drawing.Point(195, 193);
+            this.jsonClear.Name = "jsonClear";
+            this.jsonClear.Size = new System.Drawing.Size(106, 57);
+            this.jsonClear.TabIndex = 3;
+            this.jsonClear.Text = "Clear";
+            this.jsonClear.UseVisualStyleBackColor = true;
+            this.jsonClear.Click += new System.EventHandler(this.jsonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.jsonClear);
+            this.Controls.Add(this.jsonOutput);
+            this.Controls.Add(this.jsonDeserialize);
+            this.Controls.Add(this.jsonInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox jsonInput;
+        private System.Windows.Forms.Button jsonDeserialize;
+        private System.Windows.Forms.TextBox jsonOutput;
+        private System.Windows.Forms.Button jsonClear;
+    }
+}
+
---
+++ WDBChatbot/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;   
+using Newtonsoft.Json;
+using System.Windows.Forms;
+
+namespace WDBChatbot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region UI
+        private void jsonDeserialize_Click(object sender, EventArgs e)
+        {
+            //jsonDebugOutput(jsonInput.Text);
+            deserializeJson(jsonInput.Text);
+        }
+
+        private void jsonClear_Click(object sender, EventArgs e)
+        {
+            jsonInput.Text = string.Empty;
+            jsonOutput.Text = string.Empty;
+        }
+        #endregion
+
+        #region Deserialization
+        private void deserializeJson(string jsonString)
+        {
+            try
+            {
+                var jsonParse = JsonConvert.DeserializeObject<JsonClass>(jsonString);
+                jsonDebugOutput("Json object:" + jsonParse.ToString());
+                jsonDebugOutput("Top Scoring Intent is:" + jsonParse.topScoringIntent.intent.ToString());
+
+                #region Printing all elements in an array or list of Intents
+                foreach (var num in jsonParse.intents)
+                {
+                    jsonDebugOutput("Intent is " + num.intent +" and Score is "+ num.score);
+                }
+                #endregion
+
+                #region Printing all elements in array of entities
+                foreach (var num in jsonParse.entities)
+                {
+                    jsonDebugOutput("Entity selected is "+num.entity + ", type of entity is " + num.type + " and score is " + num.score);
+                }
+                #endregion
+
+            }
+            catch (Exception e)
+            {
+                jsonDebugOutput("Exception occurred:"+ e.Message.ToString());
+            }
+        }
+        #endregion
+
+        #region Output Textbox Function
+        private void jsonDebugOutput(string jsonText)
+        {
+            System.Diagnostics.Debug.Write(jsonText + Environment.NewLine);
+            jsonOutput.Text = jsonOutput.Text + jsonText + Environment.NewLine;
+            jsonOutput.SelectionStart = jsonOutput.TextLength;
+            jsonOutput.ScrollToCaret();
+        }
+        #endregion
+
+    }
+}
